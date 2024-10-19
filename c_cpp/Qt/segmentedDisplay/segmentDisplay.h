c816781939d915e496ad7d3a2aad4636af651965
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2024-10-20 00:04:28 +0200

    Qt-Painter: Drawing a seven segments (will be improved to choose between 7,11,14 & 16 segments)

---
+++ c_cpp/Qt/segmentedDisplay/segmentDisplay.h
@@ -0,0 +1,29 @@
+#ifndef SEGMENTDISPLAY_H
+#define SEGMENTDISPLAY_H
+
+#include <QWidget>
+#include <QList>
+
+class SegmentsDisplay: public QWidget {
+
+public:
+    SegmentsDisplay(QWidget *pQParent = nullptr);
+    virtual ~SegmentsDisplay() = default;
+
+    SegmentsDisplay(const SegmentsDisplay&) = delete;
+    SegmentsDisplay& operator=(const SegmentsDisplay&) = delete;
+
+    void setSegmentColor(int idx, QColor color);
+    void setPainterScaleX(qreal _scaleX);
+    void setPainterScaleY(qreal _scaleY);
+
+protected:
+    virtual void paintEvent(QPaintEvent *pQEvent) override;
+
+private:
+    QList<QColor> segsColor;
+    QList<QPainterPath> paths;
+    qreal scaleX, scaleY;
+};
+
+#endif // SEGMENTDISPLAY_H
